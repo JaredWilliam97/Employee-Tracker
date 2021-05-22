d989865b3d275615ac1730e9b8b23884d3ccdcb9
Jared Sutch <sutch002@gmail.com>
2021-05-22 14:58:07 -0500

    updated my seed sql

--- seed.sql
+++ seed.sql
@@ -12,3 +12,7 @@ INSERT INTO department (name) VALUES ("Sales");
 INSERT INTO role (title, salary, departmentID) VALUES ("Barista", 1000, 3);
 INSERT INTO role (title, salary, departmentID) VALUES ("Product Manager", 3000, 2);
 INSERT INTO role (title, salary, departmentID) VALUES ("Software Engineer", 4000, 1);
+
+INSERT INTO employee (firstName, lastName, roleID) VALUES ("Bob", "Yeet", 2);
+INSERT INTO employee (firstName, lastName, roleID) VALUES ("Alan", "Man", 4);
+INSERT INTO employee (firstName, lastName, roleID) VALUES ("Tim", "Zhou", 3);
